b0c6eb318967d7c892862708d2142531252945d6
Electric Ant <electricant@anche.no>
2016-07-18 09:54:49 +0200

    Fix wrong detection of low battery and disable the converters when external powered

--- src/serio_driver.c
+++ src/serio_driver.c
@@ -1,6 +1,6 @@
 /**
- * 
- * 
+ *
+ *
  * Copyright (C) 2015 Paolo Scaramuzza <paolo.scaramuzza@ipol.gq>
  */
 #include "include/board.h"
@@ -19,12 +19,13 @@ void serio_init()
 {
 	PORTC.DIRSET = PIN3_bm; // PIN3 (TXD0) output
 	PORTC.DIRCLR = PIN2_bm; // PIN2 (RXD0) input
-	USART_Format_Set(&USARTC0, USART_CHSIZE_8BIT_gc, USART_PMODE_DISABLED_gc, 
-								     false);
-	USART_Baudrate_Set(&USARTC0, 11, -7); // 115200 baud (see the XMEGA manual)
-	USART_Rx_Enable(&USARTC0);
-	USART_Tx_Enable(&USARTC0);
-	USART_RxdInterruptLevel_Set(&USARTC0, USART_RXCINTLVL_LO_gc);
+
+	USART_Format_Set(&SERIO_USART, USART_CHSIZE_8BIT_gc,
+		USART_PMODE_DISABLED_gc, false);
+	USART_Baudrate_Set(&SERIO_USART, 11, -7); // 115200 baud (see the XMEGA manual)
+	USART_Rx_Enable(&SERIO_USART);
+	USART_Tx_Enable(&SERIO_USART);
+	USART_RxdInterruptLevel_Set(&SERIO_USART, USART_RXCINTLVL_LO_gc);
 
 	rxBuf.next = 0;
 	rxBuf.used = 0;
@@ -33,21 +34,21 @@ void serio_init()
 }
 
 // receive data and put it in the buffer
-ISR(USARTC0_RXC_vect)
+ISR(SERIO_USART_RXC_vect)
 {
-	serio_putChar(USARTC0.DATA);
+	serio_putChar(SERIO_USART.DATA);
 }
 
 // transmit data until the buffer is empty
-ISR(USARTC0_DRE_vect)
+ISR(SERIO_USART_DRE_vect)
 {
 	if (txBuf.used == 0) {
 		// there's no need to be interrupted if nothing has to be sent
-		USART_DreInterruptLevel_Set(&USARTC0, USART_DREINTLVL_OFF_gc);
+		USART_DreInterruptLevel_Set(&SERIO_USART, USART_DREINTLVL_OFF_gc);
 	} else {
 		uint8_t index = mod(txBuf.next - txBuf.used, BUFFER_SIZE);
 		txBuf.used--;
-		USARTC0.DATA = txBuf.data[index];
+		SERIO_USART.DATA = txBuf.data[index];
 	}
 }
 
@@ -56,14 +57,14 @@ void serio_putChar(char c)
 	while (txBuf.used == BUFFER_SIZE) {;} // block until the buffer is empty
 
 	// stop interrupts when modifying the data structures
-	USART_DreInterruptLevel_Set(&USARTC0, USART_DREINTLVL_OFF_gc);
+	USART_DreInterruptLevel_Set(&SERIO_USART, USART_DREINTLVL_OFF_gc);
 
 	txBuf.data[txBuf.next] = c;
 	txBuf.next = (txBuf.next + 1) % BUFFER_SIZE;
 	txBuf.used++;
 
 	// (re)enable interrupt in order to send data
-	USART_DreInterruptLevel_Set(&USARTC0, USART_DREINTLVL_LO_gc);
+	USART_DreInterruptLevel_Set(&SERIO_USART, USART_DREINTLVL_LO_gc);
 }
 
 void serio_putString(char* string)
